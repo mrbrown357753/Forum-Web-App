67d169877b12e27fa23d080f220213fc04e9304c
Sarim Khan <sarimkhanwwe99@gmail.com>
2021-12-04 17:39:42 +0530

    added files

---
+++ MAIN/main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home, detail, posts, create_post
+urlpatterns = [
+    path("", home, name="home"),
+    path("detail/<slug>/", detail, name="detail"),
+    path("posts/<slug>/", posts, name="posts"),
+    path("create_post", create_post, name="create_post"),
+]
---
+++ MAIN/register/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms import fields
+from main.models import Author
+
+class UpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = Author
+        fields = ("fullname", "bio", "profile_pic")
